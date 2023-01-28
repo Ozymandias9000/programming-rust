bfbb93eb26636d13654933c85f0126ef7105323c
Ozymandias9000 <nick_murphy@sbcglobal.net>
2023-01-28 13:41:23 -0500

    render concurrently

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -3,6 +3,8 @@ use std::env;
 
 use utils::{parse_complex, parse_pair, render, write_image};
 
+use crate::utils::pixel_to_point;
+
 mod utils {
     mod escape_time;
     pub use escape_time::*;
@@ -39,7 +41,41 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    const USE_CONCURRENT: bool = true;
+    if USE_CONCURRENT {
+        render_concurrently(&mut pixels, bounds, upper_left, lower_right);
+    } else {
+        render(&mut pixels, bounds, upper_left, lower_right);
+    }
 
     write_image(&filename, &pixels, bounds).expect("error writing file");
+}
+
+fn render_concurrently(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: num::Complex<f64>,
+    lower_right: num::Complex<f64>,
+) {
+    let thread_count = num_cpus::get();
+    println!("{} logical cpus", thread_count);
+
+    let rows_per_band = bounds.1 / thread_count + 1;
+
+    let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+    crossbeam::scope(|spawner| {
+        for (i, band) in bands.into_iter().enumerate() {
+            let top = rows_per_band * i;
+            let height = band.len() / bounds.0;
+            let band_bounds = (bounds.0, height);
+            let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+            let band_lower_right =
+                pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+            spawner.spawn(move |_| {
+                render(band, band_bounds, band_upper_left, band_lower_right);
+            });
+        }
+    })
+    .unwrap()
 }
